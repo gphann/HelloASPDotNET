ed8b7c2bf9814770633129f4913a60d989b4b03c
Gloria <hklover@gmail.com>
2021-01-10 09:34:11 -0600

    first commit

---
+++ HelloASPDotNET/Controllers/HelloController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloASPDotNET.Controllers
+{
+    [Route("/helloworld")]
+    public class HelloController : Controller
+    {
+        //GET: /<controller>/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            string html = "<form method = 'post' action= '/hello/welcome'>"
+            string html = "<form method = 'post' action = '/hello'>"  + 
+                "<input type = 'text' name='name' />" +
+                "<input type = 'submit' value = 'Greet Me!' />" +
+            //"<label> Language: " +
+            //"<select name = 'language'>" +
+            //"<option value = '1' selected>English</option>" +
+            //"<option value = '2' selected>French</option>" +
+            //"<option value = '3' selected>Cantonese</option>" +
+            //"<option value = '4' selected>Japanese</option>" +
+            //"<option value = '5' selected>Mandarin</option>" +
+            //"</select>" + "</label>" + 
+            "</form>";
+
+            return Content(html, "text/html");
+        }
+
+        //GET: /hello/welcome
+        //[HttpGet]
+        //[Route("/helloworld/welcome/{name?}")]
+
+        [HttpGet("welcome/{name?}")]
+
+        [HttpPost]
+        [Route("/hello")]
+        //[Route("hello/welcome")] 
+            //{value?}")]
+        public IActionResult Welcome(string name = "World") //, string language)
+        {
+            return Content("<h1>Welcome to my app, " + name + "!</h1>", "text/html");
+        }
+
+        //public static CreateMessage(string name, string value)
+        //{
+        //    if (value == "1")
+        //    {
+        //        return "Hello, " + name + "!";
+        //    }
+        //    else if (value == "2")
+        //    {
+        //        return "Bonjour, " + name + "!";
+        //    }
+        //    else if (value == "3")
+        //    {
+        //        return "Lay ho, " + name + "!";
+        //    }
+        //    else if (value == "4")
+        //    {
+        //        return "Konnichiwa, " + name + "!";
+        //    }
+        //    else if (value == "5")
+        //    {
+        //        return "Ni hao, " + name + "!";
+        //    }
+        //}
+
+    }
+}
